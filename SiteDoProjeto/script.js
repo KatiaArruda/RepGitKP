10bb530fa1ebe109b8a06614ef0ec8cf29b1093c
KatiaArruda <111231609+KatiaArruda@users.noreply.github.com>
2023-02-21 18:13:05 -0300

    Alteração01

--- SiteDoProjeto/script.js
+++ SiteDoProjeto/script.js
@@ -1,1 +1,10 @@
-alert('Bem vindo!')
+function somar(){
+    let I = window.document.getElementById('I')
+    let F = window.document.querySelector('input#Fim')
+    let resposta = window.document.getElementById('resposta')
+    let I = Number(I.value)
+    let F = Number(F.value)
+    let Soma = I + F
+    resposta.innerHTML = 'A soma entre Início'
+
+}
